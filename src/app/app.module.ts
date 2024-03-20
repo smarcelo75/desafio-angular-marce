7d007e16ddf94d285c228e935bb723d113a8dd1f
Marce Serrano <smarcelo75@hotmail.com>
2024-03-20 19:32:50 -0300

    feat: Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { StudentsPageComponent } from './students-page/students-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidenavComponent,
+    StudentsPageComponent
   ],
   imports: [
     BrowserModule,
